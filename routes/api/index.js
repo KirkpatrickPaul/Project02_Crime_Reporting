a770c1bb4e244e8fa24b7d95dbba7012f4d236d7
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-12-20 11:58:52 -0600

    get, post, and put made. Need delete and error handling.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,34 +1,34 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // Requiring our models and passport as we've configured it
-const db = require('../../models');
-const passport = require('../../config/passport');
+const db = require("../../models");
+const passport = require("../../config/passport");
 
 // Using the passport.authenticate middleware with our local strategy.
 // If the user has valid login credentials, send them to the members page.
 // Otherwise the user will be sent an error
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post("/login", passport.authenticate("local"), (req, res) => {
   // Sending back a password, even a hashed password, isn't a good idea
   res.json({
     email: req.user.email,
-    id: req.user.id
+    id: req.user.id,
   });
 });
 
 // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
 // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
 // otherwise send back an error
-router.post('/signup', (req, res) => {
+router.post("/signup", (req, res) => {
   db.User.create(req.body)
     .then(() => {
-      res.redirect(307, '/api/login');
+      res.redirect(307, "/api/login");
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(401).json(err);
     });
 });
 
 // Route for getting some data about our user to be used client side
-router.get('/user_data', (req, res) => {
+router.get("/user_data", (req, res) => {
   if (!req.user) {
     // The user is not logged in, send back an empty object
     return res.json({});
@@ -38,5 +38,29 @@ router.get('/user_data', (req, res) => {
   const { password, ...user } = req.user;
   res.json(user);
 });
+
+// Above this line is boilerplate code.
+router
+  .route("/crimes/:id?")
+
+  .get(async (req, res) => {
+    let criteria = { include: [db.User] };
+    if (req.params.id) {
+      criteria.where = { id: req.params.id };
+    }
+    const crimes = await db.Crime.findAll(criteria);
+    res.status(200).json(crimes);
+  })
+
+  .post(async (req, res) => {
+    const newCrime = await db.Crime.create(req.body);
+    res.status(201).json(newCrime);
+  })
+
+  .put(async (req, res) => {
+    const criteria = { where: { id: req.params.id }, ...req.body };
+    const updated = await db.Crime.update(criteria);
+    res.status.json(criteria);
+  });
 
 module.exports = router;
