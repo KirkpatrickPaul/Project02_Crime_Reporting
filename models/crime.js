519a39de47fc9a920d5ae5986c45b00c40c8ed5e
Seth Glenn <Sbgees@yahoo.com>
2020-12-21 23:50:00 -0600

    fixed lint issues

--- models/crime.js
+++ models/crime.js
@@ -1,37 +1,37 @@
-module.export = function(sequelize, DataTypes) {
+module.export = function (sequelize, DataTypes) {
   const Crime = sequelize.define('Crime', {
     user: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
+      unique: true
     },
     title: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1],
+        len: [1]
       },
     },
     body: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1],
+        len: [1]
       },
     },
     longitude: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     latitude: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
   });
-  Crime.associate = function(models) {
+  Crime.associate = function (models) {
     Crime.belongsto(models.User, {
       foreignkey: {
-        allowNull: false,
+        allowNull: false
       },
     });
   };
