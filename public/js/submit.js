f1bf26037360d93e13a96bfa0742cbe67a3fa491
Seth Glenn <Sbgees@yahoo.com>
2020-12-26 19:16:49 -0600

    created function to get crimes

--- public/js/submit.js
+++ public/js/submit.js
@@ -1,4 +1,43 @@
-// Code to submit crimes
-$(document).ready(function(){
-    
+// Event handlers to submit crimes
+$(document).ready(() => {
+    const crimeInput = $();
+    const crimeList = $();
+    const crimeContainer = $();
+
+    $(document).on("submit", "", )
+
+    function getCrimes () {
+        $.get("/api/crimes", function(data){
+            let rowsToAdd = [];
+            for (var i = 0; i < data.length; i++){
+                rowsToAdd.push(createCrimeRow(data[i]));
+            }
+            renderCrimeList(rowsToAdd);
+            nameInput.val("");
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
