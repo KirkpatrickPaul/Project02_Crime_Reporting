73905e1591e22e87eb821b52bc3dcc8dbad0d6bc
Seth Glenn <Sbgees@yahoo.com>
2020-12-27 16:21:10 -0600

    creating functions to concatenate html to display to webpage

--- public/js/submit.js
+++ public/js/submit.js
@@ -1,51 +1,86 @@
 // Event handlers to submit crimes
 $(document).ready(() => {
-    const crimeInput = $();
-    const crimeList = $();
-    const crimeContainer = $();
+  const crimeInput = $();
+  const crimeList = $();
+  const crimeContainer = $();
 
-    $(document).on("submit", "", )
+  // Event listeners for posting and deleting crimes
+  $(document).on('submit', '#crime-form', handleCrimeFormSubmit);
+  $(document).on('submit', '#delete-crime', crimeDelete);
 
-    // Getting initial list of Crimes
-    getCrimes();
-    // Function to get the Crimes getting them ready for rendering
-    function getCrimes () {
-        $.get("/api/crimes", function(data){
-            let rowsToAdd = [];
-            for (var i = 0; i < data.length; i++){
-                rowsToAdd.push(createCrimeRow(data[i]));
-            }
-            renderCrimeList(rowsToAdd);
-            nameInput.val("");
-        });
+  // Getting initial list of Crimes
+  getCrimes();
+  // Function to handle crime post button submit
+  function handleCrimeFormSubmit(event) {
+    event.preventDefeault();
+
+    if (
+      !nameInput
+        .val()
+        .trim()
+        .trim()
+    ) {
+      return;
     }
-    // Function for rendering the list of Crimes
-    function renderCrimeList(rows) {
-        crimeList.children().not(":last").remove();
-        crimeContainer.children(".alert").remove();
-        if (rows.length) {
-            console.log(rows);
-            crimeList.prepend(rows);
-        }
-        else {
-            renderEmpty();
-        }
+    upsertCrime({
+      name: nameInput.val().trim()
+    });
+  }
+  // function for creating a crime. Calls getCrimes when completed
+  function upsertCrime(crimeData) {
+    $.post('/api/crimes', crimeData).then(getCrimes);
+  }
+  // Function for creating new list of Crimes
+  function createCrimeRow(crimeData) {
+    let newTr = $("<tr>");
+    newTr.data("crime", crimeData);
+    newTr.append("<td>" + crimeData.name + "</td>");
+    if (crimeData.Posts) {
+        
     }
-    // Function for rendering an empty Crime input
-    function renderEmpty() {
-        let alertDiv = $("<div>");
-        alertDiv.addClass("alert alert-danger");
-        alertDiv.text("You must create a Crime before you can post it.");
-        crimeContainer.append(alertDiv);
+  }
+  // Function to get the Crimes getting them ready for rendering
+  function getCrimes() {
+    $.get('/api/crimes', function(data) {
+      let rowsToAdd = [];
+      for (var i = 0; i < data.length; i++) {
+        rowsToAdd.push(createCrimeRow(data[i]));
+      }
+      renderCrimeList(rowsToAdd);
+      nameInput.val('');
+    });
+  }
+  // Function for rendering the list of Crimes
+  function renderCrimeList(rows) {
+    crimeList
+      .children()
+      .not(':last')
+      .remove();
+    crimeContainer.children('.alert').remove();
+    if (rows.length) {
+      console.log(rows);
+      crimeList.prepend(rows);
+    } else {
+      renderEmpty();
     }
-    // Function for deleting Crime inputs
-    function crimeDelete(){
-        const listItemData = $(this).parent("td").parent("tr").data("crime");
-        const id = listItemData;
-        $.ajax({
-            method: "DELETE",
-            url: "/api/crimes/" + id
-        })
-            .then(getCrimes);
-    }
+  }
+  // Function for rendering an empty Crime input
+  function renderEmpty() {
+    let alertDiv = $('<div>');
+    alertDiv.addClass('alert alert-danger');
+    alertDiv.text('You must create a Crime before you can post it.');
+    crimeContainer.append(alertDiv);
+  }
+  // Function for deleting Crime inputs
+  function crimeDelete() {
+    const listItemData = $(this)
+      .parent('td')
+      .parent('tr')
+      .data('crime');
+    const id = listItemData;
+    $.ajax({
+      method: 'DELETE',
+      url: '/api/crimes/' + id
+    }).then(getCrimes);
+  }
 });
