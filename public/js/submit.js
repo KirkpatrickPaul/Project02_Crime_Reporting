e12525e35d7a18c883f32c5cf74ce02bfcd9faa3
Seth Glenn <Sbgees@yahoo.com>
2020-12-28 20:32:17 -0600

    lint issues

--- public/js/submit.js
+++ public/js/submit.js
@@ -1,5 +1,5 @@
 // Event handlers to submit crimes
-$(document).ready(() => {
+$(document).ready( () => {
   const crimeInput = $();
   const crimeList = $();
   const crimeContainer = $();
@@ -11,7 +11,7 @@ $(document).ready(() => {
   // Getting initial list of Crimes
   getCrimes();
   // Function to handle crime post button submit
-  function handleCrimeFormSubmit(event) {
+  function handleCrimeFormSubmit (event) {
     event.preventDefeault();
 
     if (
@@ -27,27 +27,32 @@ $(document).ready(() => {
     });
   }
   // function for creating a crime. Calls getCrimes when completed
-  function upsertCrime(crimeData) {
+  function upsertCrime (crimeData) {
     $.post('/api/crimes', crimeData).then(getCrimes);
   }
   // Function for creating new list of Crimes
-  function createCrimeRow(crimeData) {
-    let newTr = $("<tr>");
-    newTr.data("crime", crimeData);
-    newTr.append("<td>" + crimeData.name + "</td>");
+  function createCrimeRow (crimeData) {
+    let newTr = $('<tr>');
+    newTr.data('crime', crimeData);
+    newTr.append('<td>' + crimeData.name + '</td>');
     if (crimeData.Posts) {
-        newTr.append("<td>" + crimeData.Posts.length + "</td>");
+      newTr.append('<td>' + crimeData.Posts.length + '</td>');
+    } else {
+      newTr.append('<td>0</td>');
     }
-    else {
-        newTr.append("<td>0</td>")
-    }
-    newTr.append("<td><a href='/blog?crime_id=" + crimeData.id + "'>Go to Crimes</a></td>");
-    newTr.append("<td><a href='/cms?crime_id=" + crimeData.id + "'>Create a Crime</a></td>");
-    newTr.append("<td><a style='cursor:pointer;color:red' class='delete-crime'>Delete Crime</a></td>");
+    newTr.append(
+      "<td><a href='/blog?crime_id=" + crimeData.id + "'>Go to Crimes</a></td>"
+    );
+    newTr.append(
+      "<td><a href='/cms?crime_id=" + crimeData.id + "'>Create a Crime</a></td>"
+    );
+    newTr.append(
+      "<td><a style='cursor:pointer;color:red' class='delete-crime'>Delete Crime</a></td>"
+    );
     return newTr;
   }
   // Function to get the Crimes getting them ready for rendering
-  function getCrimes() {
+  function getCrimes () {
     $.get('/api/crimes', function(data) {
       let rowsToAdd = [];
       for (var i = 0; i < data.length; i++) {
@@ -58,7 +63,7 @@ $(document).ready(() => {
     });
   }
   // Function for rendering the list of Crimes
-  function renderCrimeList(rows) {
+  function renderCrimeList (rows) {
     crimeList
       .children()
       .not(':last')
@@ -72,14 +77,14 @@ $(document).ready(() => {
     }
   }
   // Function for rendering an empty Crime input
-  function renderEmpty() {
+  function renderEmpty () {
     let alertDiv = $('<div>');
     alertDiv.addClass('alert alert-danger');
     alertDiv.text('You must create a Crime before you can post it.');
     crimeContainer.append(alertDiv);
   }
   // Function for deleting Crime inputs
-  function crimeDelete() {
+  function crimeDelete () {
     const listItemData = $(this)
       .parent('td')
       .parent('tr')
