29a46a48d423d9f545933d9da74e6e4fe8744b71
Seth Glenn <Sbgees@yahoo.com>
2021-01-02 13:26:07 -0600

    implementing marker clusters for google maps API

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -10,4 +10,19 @@ function initMap() {
     position: kansasCity,
     map: map
   });
+  const markers = locations.map((location, i) => {
+    return new google.maps.Marker({
+      position: location,
+      // label: labels[i % labels.length]
+    });
+  });
+  new MarkerClusterer(map, markers, {
+    imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
 }
+const locations = [
+  { lat: 38.980509171366094, lng: -94.69020214361967},
+  { lat: 38.98130945250535, lng: -94.69007114287267},
+  { lat: 38.98019187382965, lng: -94.68945155264056},
+  { lat: 38.979419841460995, lng: -94.68740563598648}
+]
