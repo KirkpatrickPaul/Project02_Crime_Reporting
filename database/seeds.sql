db906edb10dd5044f92f8694ed8f956b3d0752a6
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2021-01-01 23:11:16 -0600

    markers are working.

--- database/seeds.sql
+++ database/seeds.sql
@@ -3,11 +3,11 @@ INSERT INTO users (email, password, createdAt, UpdatedAt) VALUES ("Kelsey@Eckelb
 INSERT INTO users (email, password, createdAt, UpdatedAt) VALUES ("Seth@Glenn.com", "password", '2020-12-31 09:43:13', '2020-12-31 09:43:13');
 INSERT INTO users (email, password, createdAt, UpdatedAt) VALUES ("Anthony@Gamon.com", "password", '2020-12-31 09:43:13', '2020-12-31 09:43:13');
 
-INSERT INTO crimes (UserId, title, body, longitude, latitude, createdAt, UpdatedAt) 
-	VALUES (1, "Oatmeal Thief Hits Downtown", "The oatmeal thief has struck again! I was making my oatmeal this morning, when BAM! it was just gone!", 38.980509171366094, -94.69020214361967, '2021-01-02 09:43:13', '2021-01-02 09:43:13');
-INSERT INTO crimes (UserId, title, body, longitude, latitude, createdAt, UpdatedAt) 
-	VALUES (2, "Sidewalks Vandalized with Multiple Hopscotch Drawings", "I came outside this morning to see the entire sidewalk just completely ruined. Some gang members or something drew disturbing occult symbols all over them.", 38.98130945250535, -94.69007114287267, '2021-01-02 12:43:13', '2021-01-02 12:43:13');
-INSERT INTO crimes (UserId, title, body, longitude, latitude, createdAt, UpdatedAt) 
-	VALUES (3, "House fire on Empty Lot", "I know the lot is an empty slab of concrete, but I saw a guy smoking over there and if he would have been in a house, it totally would have caught fire.", 38.98019187382965, -94.68945155264056, '2021-01-04 15:44:13', '2021-01-02 15:44:13');
-INSERT INTO crimes (UserId, title, body, longitude, latitude, createdAt, UpdatedAt) 
-	VALUES (4, "Shoes Spray Painted on Side of House", "I'm not sure who did it, but it can't be good. Someone was painting shoes on houses. And these shoes had to be huge, I mean like at least size 50 or something. I don't know what wears shoes like that, but whatever it is, I don't want to mess with it.", 38.979419841460995, -94.68740563598648, '2021-01-05 09:43:13', '2021-01-05 09:43:13');
+INSERT INTO crimes (UserId, title, body, latitude, longitude, createdAt, UpdatedAt) 
+	VALUES (1, "Oatmeal Thief Hits Downtown", "The oatmeal thief has struck again! I was making my oatmeal this morning, when BAM! it was just gone!", 39.099930, -94.582493, '2021-01-02 09:43:13', '2021-01-02 09:43:13');
+INSERT INTO crimes (UserId, title, body, latitude, longitude, createdAt, UpdatedAt) 
+	VALUES (2, "Sidewalks Vandalized with Multiple Hopscotch Drawings", "I came outside this morning to see the entire sidewalk just completely ruined. Some gang members or something drew disturbing occult symbols all over them.", 39.009472, -94.446524, '2021-01-02 12:43:13', '2021-01-02 12:43:13');
+INSERT INTO crimes (UserId, title, body, latitude, longitude, createdAt, UpdatedAt) 
+	VALUES (3, "House fire on Empty Lot", "I know the lot is an empty slab of concrete, but I saw a guy smoking over there and if he would have been in a house, it totally would have caught fire.", 38.980191873, -94.689451552, '2021-01-04 15:44:13', '2021-01-02 15:44:13');
+INSERT INTO crimes (UserId, title, body, latitude, longitude, createdAt, UpdatedAt) 
+	VALUES (4, "Shoes Spray Painted on Side of House", "I'm not sure who did it, but it can't be good. Someone was painting shoes on houses. And these shoes had to be huge, I mean like at least size 50 or something. I don't know what wears shoes like that, but whatever it is, I don't want to mess with it.", 39.166900, -94.589001, '2021-01-05 09:43:13', '2021-01-05 09:43:13');
