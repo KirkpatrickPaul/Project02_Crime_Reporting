802724b3c88fd687d69ac3bed54455022c04595a
Anthony Gamon <anthonygamon@Anthonys-MBP-2.attlocal.net>
2021-01-02 10:03:22 -0600

    working on page styling

--- public/js/members.js
+++ public/js/members.js
@@ -1,3 +1,15 @@
+function initMap() {
+  const kansasCity = { lat: 39.099728, lng: -94.578568 };
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: kansasCity
+  });
+  const marker = new google.maps.Marker({
+    position: kansasCity,
+    map: map
+  });
+}
+
 const $ = window.$;
 
 $(document).ready(() => {
