2139c39ced0199cfcf975b6823970d4b433ec7a6
Seth Glenn <Sbgees@yahoo.com>
2021-01-02 23:55:23 -0600

    maps

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,17 +1,6 @@
 /* eslint-disable */
 // Initialize and add the map
 
-const markerCluster = new MarkerClusterer(map, markers, {
-  imagePath: `${path}/m`
-});
-
-const locations = [
-  { lat: 38.980509171366094, lng: -94.69020214361967 },
-  { lat: 38.98130945250535, lng: -94.69007114287267 },
-  { lat: 38.98019187382965, lng: -94.68945155264056 },
-  { lat: 38.979419841460995, lng: -94.68740563598648 }
-];
-
 // function to initialize map and display coordinates
 function initMap() {
   const kansasCity = { lat: 39.099728, lng: -94.578568 };
@@ -19,10 +8,11 @@ function initMap() {
     zoom: 12,
     center: kansasCity
   });
-  const marker = new google.maps.Marker({
-    position: kansasCity,
-    map: map
-  });
+  // const marker = new google.maps.Marker({
+  //   position: kansasCity,
+  //   map: map
+  // });
+  // cluster code integrated into map
   const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   const markers = locations.map((location, i) => {
     return new google.maps.Marker({
@@ -30,8 +20,13 @@ function initMap() {
       label: labels[i % labels.length]
     });
   });
-  new MarkerClusterer(map, markers, {
-    imagePath:
-      'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+  const markerCluster = new MarkerClusterer(map, markers, {
+    imagePath: './photos'
   });
 }
+let locations = [
+  { lat: 38.980509171366094, lng: -94.69020214361967 },
+  { lat: 38.98130945250535, lng: -94.69007114287267 },
+  { lat: 38.98019187382965, lng: -94.68945155264056 },
+  { lat: 38.979419841460995, lng: -94.68740563598648 }
+];
