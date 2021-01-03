e9924794fb607bf207d688ca3e0b42b1dca29d4e
Seth Glenn <Sbgees@yahoo.com>
2021-01-03 02:08:56 -0600

    created submitCrime function

--- public/js/crimesubmit.js
+++ public/js/crimesubmit.js
@@ -8,8 +8,14 @@ $(document).ready(() => {
     const crimeLocation = $("#crimeLoc");
 
     // Event listeners for posting, updating, and deleting crimes
-    $(document).on("click",);
+    $(document).on("click","#crimeSubmit", submitCrime);
     $(document).on("click",);
     $(document).on("click",);
 
+    // function to submit crime and take user to crime page
+    function submitCrime(post){
+        $.post("/api/crimes", post, () => {
+            window.location.href = "/crime"
+        });
+    }
 })
