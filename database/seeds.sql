f45db9d5b7be2ed68fa1ae0ad021ffa912021f45
Anthony Gamon <anthonygamon@Anthonys-MBP-2.attlocal.net>
2021-01-02 23:12:09 -0600

    added to sql

--- database/seeds.sql
+++ database/seeds.sql
@@ -11,4 +11,17 @@ INSERT INTO crimes (UserId, title, body, latitude, longitude, createdAt, Updated
 	VALUES (3, "House fire on Empty Lot", "It was so weird seeing an empty lot burn like that.", 38.980191873, -94.689451552, '2021-01-04 15:44:13', '2021-01-02 15:44:13');
 INSERT INTO crimes (UserId, title, body, latitude, longitude, createdAt, UpdatedAt) 
 	VALUES (4, "Shoes Spray Painted on Side of House", "Someone was painting shoes on houses and these shoes are huge.", 39.166900, -94.589001, '2021-01-05 09:43:13', '2021-01-05 09:43:13');
+INSERT INTO crimes (UserId, title, body, latitude, longitude, createdAt, UpdatedAt) 
+	VALUES (1, "Missing Bacon", "Someone has stolen my bacon out of my refrigerator", 39.098920, -94.556090, '2021-01-05 09:43:13', '2021-01-05 09:43:13');
+INSERT INTO crimes (UserId, title, body, latitude, longitude, createdAt, UpdatedAt) 
+	VALUES (2, "Missing piggy?", "I found a little piggy with a collar that reads Piggy Smalls. No address listed.", 38.917290, -94.648360, '2021-01-05 09:43:13', '2021-01-05 09:43:13');
+INSERT INTO crimes (UserId, title, body, latitude, longitude, createdAt, UpdatedAt) 
+	VALUES (3, "Stolen vending machine", "Police are investigating a stolen vending machine from the first floor", 39.040250, -94.597470, '2021-01-05 09:43:13', '2021-01-05 09:43:13');
+INSERT INTO crimes (UserId, title, body, latitude, longitude, createdAt, UpdatedAt) 
+	VALUES (4, "Attacked during our bbq", "Please keep an eye out for the squirls in the neighborhood, I was attacked at our bbq last night by one. He stole my hamburger and chips.", 39.042370, -94.600662, '2021-01-05 09:43:13', '2021-01-05 09:43:13');
+INSERT INTO crimes (UserId, title, body, latitude, longitude, createdAt, UpdatedAt) 
+	VALUES (1, "Stolen car", "Someone stole my car last night around 2 and 3 am. It is a grey Honda Civic with 4 doors.", 39.000450, -94.694350, '2021-01-05 09:43:13', '2021-01-05 09:43:13');
+INSERT INTO crimes (UserId, title, body, latitude, longitude, createdAt, UpdatedAt) 
+	VALUES (2, "Suspicious coin", "Turns out it was only a quarter.", 38.973691, -94.675036, '2021-01-05 09:43:13', '2021-01-05 09:43:13');
+	
 
